517684bb04ae55010cc7b9d8bdf3875b614bec5a
vicky <lch82327@gmail.com>
2016-08-26 11:35:45 +0800

    add statci page and modify some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
+  root to: redirect('/articles')
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
